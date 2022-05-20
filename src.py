af451ba3939221c7642052a68d0fe327d4b6e766
horn553 <36446753+horn553@users.noreply.github.com>
2022-05-20 20:23:09 +0900

    isort 対応のフォークに更新

--- src.py
+++ src.py
@@ -1,1 +1,13 @@
-print('code satisfying lake8')
+# isort に違反したコード
+import re
+import abc
+
+type(re)
+type(abc)
+
+# flake8 に違反したコード
+print('long long long long long long long long long long long long long long code')
+
+# mypy に違反したコード
+number: int = 100
+list(number)
