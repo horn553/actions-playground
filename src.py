8dd8c793554ccde8ea9d9dacf3b5ec40047fb6d1
horn553 <36446753+horn553@users.noreply.github.com>
2022-05-23 21:43:58 +0900

    Linterそれぞれ独立したjobとして実装

--- src.py
+++ src.py
@@ -1,1 +1,13 @@
-print('code satisfying lake8')
+# isort で整列されたコード
+import abc
+import re
+
+type(abc)
+type(re)
+
+# flake8 を通過するコード
+print('short line')
+
+# mypy を通過するコード
+number: int = 100
+number = 200
