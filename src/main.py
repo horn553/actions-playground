17bd63a7dd082506cc1bffb0315c8e79c179fcbb
horn553 <36446753+horn553@users.noreply.github.com>
2022-05-23 21:48:30 +0900

    isort, flake8, mypy 全てに違反するコードを用意

--- src/main.py
+++ src/main.py
@@ -1,13 +1,14 @@
-# isort で整列されたコード
-import abc
+# isort で整列されていないコード
 import re
+import abc
 
 type(abc)
 type(re)
 
-# flake8 を通過するコード
+# flake8 を通過しないコード
 print('short line')
+print('long long long long long long long long long long long long long long line')
 
-# mypy を通過するコード
+# mypy を通過しないコード
 number: int = 100
-number = 200
+number = '200'
